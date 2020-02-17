fbca20de8a3e354d10c112e4bd0449aae0a6e056
Pierre Bretéché <pbreteche@dawan.fr>
2020-02-17 12:14:53 +0100

    Gestion page 404

--- ArticleProvider.php
+++ ArticleProvider.php
@@ -5,6 +5,10 @@ class ArticleProvider
 
     public function getArticle()
     {
-        return 'Mon premier article';
+        if ($_SERVER['REQUEST_URI'] === '/premier-article') {
+            return 'Mon premier article';
+        }
+        http_response_code(404);
+        return 'Article non-trouvé';
     }
 }
