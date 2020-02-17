83bf2edc87534879282d0a1706df01ffe9dd7611
Pierre Bretéché <pbreteche@dawan.fr>
2020-02-17 14:25:28 +0100

    déplacement du rendu HTML dans un fichier séparé

--- HTMLFormatter.php
+++ HTMLFormatter.php
@@ -4,17 +4,8 @@ class HTMLFormatter
 {
     public function format($article)
     {
-      return  <<<EOT
-<!Doctype>
-<html>
-<head>
-    <title>{$article['title']}</title>
-</head>
-<body>
-    <h1>{$article['title']}</h1>
-    <div>{$article['content']}</div>
-</body>
-</html>
-EOT;
+        ob_start(); // capture de toute écriture dans le document
+        include 'article.html.php';
+        return ob_get_clean(); // récupère le contenu puis vide le tampon de sortie
     }
 }
