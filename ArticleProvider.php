9137bdaa955563768383b8fc949ea8468a6ffaee
Pierre Bretéché <pbreteche@dawan.fr>
2020-02-17 14:02:16 +0100

    exemple chargement du json

--- ArticleProvider.php
+++ ArticleProvider.php
@@ -7,6 +7,7 @@ class ArticleProvider
     public function __construct()
     {
         $this->articles = include 'article_list.php';
+        $this->articles = json_decode(file_get_contents('article_list.json'), true);
     }
 
     public function getArticle(string $slug)
