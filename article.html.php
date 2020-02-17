915816184097a01ec16c13ed2ca9e8787851e4ba
Pierre Bretéché <pbreteche@dawan.fr>
2020-02-17 14:32:08 +0100

    protection anti XSS

--- article.html.php
+++ article.html.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $article['title'] ?></title>
+    <title><?= htmlspecialchars($article['title']) ?></title>
 </head>
 <body>
-<h1><?= $article['title'] ?></h1>
-<div><?= $article['content'] ?></div>
+<h1><?= htmlspecialchars($article['title']) ?></h1>
+<div><?= htmlspecialchars($article['content']) ?></div>
 </body>
 </html>
