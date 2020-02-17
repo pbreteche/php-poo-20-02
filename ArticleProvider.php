2e93dbb61f83e026d049d56eb4e0609602ebe8c0
Pierre Bretéché <pbreteche@dawan.fr>
2020-02-17 14:22:10 +0100

    test injection HTML (XSS)

--- ArticleProvider.php
+++ ArticleProvider.php
@@ -6,8 +6,8 @@ class ArticleProvider
 
     public function __construct()
     {
-        $this->articles = include 'article_list.php';
         $this->articles = json_decode(file_get_contents('article_list.json'), true);
+        $this->articles = include 'article_list.php';
     }
 
     public function getArticle(string $slug)
